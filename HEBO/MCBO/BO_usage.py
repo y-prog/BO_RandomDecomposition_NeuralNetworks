3b1401a878c4f59ad3e0a9545f0c50fdf8bcedd0
y-prog <ydg886@gmail.com>
2024-03-17 22:19:12 +0100

    modified eggholder

--- HEBO/MCBO/BO_usage.py
+++ HEBO/MCBO/BO_usage.py
@@ -9,15 +9,25 @@ import numpy as np
 
 
 # Define the Eggholder function
-def eggholder(x, y):
-    return (-(y + 47) * np.sin(np.sqrt(np.abs(x / 2 + (y + 47)))) - x * np.sin(np.sqrt(np.abs(x - (y + 47)))))
+#def eggholder(x, y):
+ #   return (-(y + 47) * np.sin(np.sqrt(np.abs(x / 2 + (y + 47)))) - x * np.sin(np.sqrt(np.abs(x - (y + 47)))))
 
-# Define the range of x and y values
-x_min, x_max = -512, 512
-y_min, y_max = -512, 512
+def eggholder(x, y):
+    return (-(y - 5) * np.cos(np.sqrt(abs(x / 2 + (y + 1)) * 20)) - x * np.cos(np.sqrt(abs(x - (y + 50)) * 10))) ** 2
+
+    #return (-(y + 47) * np.sin(np.sqrt(abs(x / 20 + (y + .047)))) - x * np.sin(np.sqrt(abs(x - (y + 47))))) ** 2
+    #return (-(y + 47) * np.sin(np.sqrt(abs(x / 2 + (y + 4.7)))) - x * np.sin(np.sqrt(abs(x - (y + 47))))) ** 2
+    #return (-(y - 5) * np.sin(np.sqrt(abs(x/2 + (y + 1))*20)) - x * np.cos(np.sqrt(abs(x - (y +50))*10)))**2
+
+lims=300
+x_min, x_max = -lims, lims
+y_min, y_max = -lims, lims
 
 # Define the number of points per dimension
-num_points_per_dimension =25
+num_points_per_dimension =30
+
+
+
 
 # Generate X1, X2, X3, and X4 coordinates with the specified number of points
 x = np.linspace(x_min, x_max, num_points_per_dimension)
@@ -50,11 +60,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 
 
 # Usage:
-layer_sizes = [4,5,3,1]   # Define your layer sizes
+layer_sizes = [4,4,1]   # Define your layer sizes
 
 flex_NN_obj_train = flex_NN(layer_sizes, X_train, y_train)
 activation_obj = activation_functions()
-
 parameters = flex_NN_obj_train.label_parameters()
 
 hidden_activation = activation_obj.relu
@@ -65,13 +74,11 @@ task = CustomTask(layer_sizes, X_train, y_train)  # Instantiate CustomTask from 
 searchspace = task.get_search_space()
 
 optimizer_builder = BoBuilder(model_id='gp_rd', acq_opt_id='is', acq_func_id='ei', tr_id='basic')
-opt = optimizer_builder.build_bo(search_space=searchspace, n_init=300)
+opt = optimizer_builder.build_bo(search_space=searchspace, n_init=100)
 print('opt ========', opt)
 
 weight_bias_optimizer = WeightAndBiasOptimizer(task=task, optimizer=opt)
 
-all_weights_and_biases_and_loss_coeff= weight_bias_optimizer.weights_biases_loss_coeff(
-                                layer_sizes, 250, X_train, y_train, loss_fn_train)
 
 # Call the weights_biases_loss_coeff function to generate weights_and_biases_list and loss_coeff_list
 weights_and_biases_list, loss_coeff_list =\
@@ -87,7 +94,6 @@ print(best_weights_and_biases, best_loss_coeff)
 
 flex_NN_obj_test=flex_NN(layer_sizes, X_test, y_test)
 ML_model_preds = flex_NN_obj_test.forward_prop(hidden_activation, output_activation, best_weights_and_biases)
-
 
 loss_fn_test = loss_functions(y_test).accuracy(ML_model_preds)
 print(loss_fn_test)
